9d79d4d23c8232576123d6c6980793de475ba4e7
FrancoUtn <francosardiutn@gmail.com>
2025-04-23 09:10:55 -0300

    Subo proyecto levantado con Grade-groovy

--- Trabajo Practico/TpTodoApp/src/models/backlog.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const backlogSchema = new mongoose.Schema({
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }]
-});
-
-module.exports = mongoose.model("Backlog", backlogSchema);
